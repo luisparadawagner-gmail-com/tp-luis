6b1223195100e267a5333349f73bd00a7896b883
LUIS ALBERTO PARADA <luisparadawagner@gmail.com>
2020-04-09 01:09:45 -0300

    Se crea componente alta-alumno

---
+++ src/app/alumnos/alta-alumnos/alta-alumnos.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-alta-alumnos',
+	templateUrl: './alta-alumnos.component.html'
+})
+export class AltaAlumnosComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { AltaAlumnosComponent } from './alumnos/alta-alumnos/alta-alumnos.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, AltaAlumnosComponent ],
+	imports: [ BrowserModule, AppRoutingModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
